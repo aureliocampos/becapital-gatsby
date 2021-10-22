e1a19e4f01c64fd83a1642f858766badff08889c
JoaoPBarros <joaopedrobarros5@gmail.com>
2021-10-22 16:30:14 -0300

    integration with wordpress

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/pages/404.js")),
+  "component---src-pages-blog-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/pages/blog.js")),
+  "component---src-pages-carreiras-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/pages/carreiras.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/pages/index.js")),
+  "component---src-pages-somos-becapital-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/pages/somos-becapital.js")),
+  "component---src-templates-blog-post-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/templates/blog-post.js")),
+  "component---src-templates-service-post-js": preferDefault(require("/home/joao/dev/becapital-wp-integrado/becapital-gatsby/src/templates/service-post.js"))
+}
+
